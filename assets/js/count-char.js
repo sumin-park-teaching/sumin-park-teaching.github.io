4eb76ae074af31dcb726827c26861f3df481f621
Sumin Park <144093440+sumin-park-teaching@users.noreply.github.com>
2026-05-04 07:46:43 +0000

    한글 글자 비율 계산

--- assets/js/count-char.js
+++ assets/js/count-char.js
@@ -11,3 +11,18 @@ for (const ch of text) {
 
 console.log(`${text}`)
 console.log(`'${target}' 글자는 ${count} 번 등장합니다.`)
+
+let hangul = 0; // 한글이 몇 글자인가?
+let total = 0; // 전체 텍스트가 몇 글자인가?
+
+for (const ch of text) {
+    if (ch !== " ") { // 공백 문자가 아닌 경우
+        total++; // 전체 글자 수에 포함
+        if (ch >= "가" && ch <= "힣") { // 한글인 경우
+            hangul++;
+        }
+    }
+}
+
+const ratio = (hangul / total) * 100;
+console.log(`한글 글자 비율: ${ratio.toFixed(1)}`);
