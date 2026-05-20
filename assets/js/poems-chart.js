821a9c985bf6a86f86d5a85b4b417edb3585a733
Sumin Park <144093440+sumin-park-teaching@users.noreply.github.com>
2026-05-20 07:36:29 +0000

    두 번째 차트: 색깔 변경

--- assets/js/poems-chart.js
+++ assets/js/poems-chart.js
@@ -25,7 +25,16 @@ function drawChart(rows) {
         type: "bar",
         data: {
             labels: labels,
-            datasets: [{label: "작품 편수", data: counts}]
+            datasets: [{
+                label: "작품 편수",
+                data: counts,
+                backgroundColor: [
+                    "rgba(255, 99, 132, 0.6)", // 분홍
+                    "rgba(54, 162, 235, 0.6)", // 파랑
+                    "rgba(255, 206, 86, 0.6)", // 노랑
+                    "rgba(75, 192, 192, 0.6)" // 청록
+                ]
+            }]
         }
     });
 }
